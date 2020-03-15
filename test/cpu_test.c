1b9c898c15ab3b8d022b3cb06b633c436bf7840c
nickr23 <nickrich.gmail.com>
2020-03-14 20:27:14 -0400

    Added get16reg test

--- test/cpu_test.c
+++ test/cpu_test.c
@@ -18,10 +18,28 @@ void test_initCPU(void ** state) {
   assert_true(cpu.pc == 0x0000);
 }
 
+void test_getRegister16(void ** state) {
+  CPU cpu = initCPU();
+  
+  cpu.af = 0x1234;
+  cpu.bc = 0x8765;
+  cpu.de = 0x5FA1;
+  cpu.hl = 0x7DD1;
+  cpu.sp = 0x5766;
+  cpu.pc = 0xABCD;
+  assert_true(cpu.af == 0x1234);
+  assert_true(cpu.bc == 0x8765);
+  assert_true(cpu.de == 0x5FA1);
+  assert_true(cpu.hl == 0x7DD1);
+  assert_true(cpu.sp == 0x5766);
+  assert_true(cpu.pc == 0xABCD);
+}
+
 int main (void) {
   const struct CMUnitTest tests [] =
   {
-    cmocka_unit_test (test_initCPU),
+    cmocka_unit_test(test_initCPU),
+    cmocka_unit_test(test_getRegister16),
   };
 
   int count_fail_tests = cmocka_run_group_tests (tests, NULL, NULL);
